bb94390f026990dd90a8dad8bfc14fe659b7aa85
Matheus Del Valle <matheussdv@hotmail.com>
2018-06-21 10:39:56 -0300

    First commit

---
+++ App1/App1/Views/AboutPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Lottie.Forms;
+using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage ()
+		{
+			InitializeComponent ();
+
+            var animationView = new AnimationView()
+            {
+
+            };
+        }
+	}
+}
